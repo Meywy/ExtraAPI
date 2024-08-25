75a52ba21ee3ea1595fffcd5b22ae3577a870887
meywy <meywyy@gmail.com>
2024-08-25 15:22:11 +0200

    Initial commit

---
+++ src/main/java/dev/meywy/extraAPI/ExtraAPI.java
@@ -0,0 +1,13 @@
+package dev.meywy.extraAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExtraAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
